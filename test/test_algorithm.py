4a3881829fde92812208d7032ad6964700bb5c72
Igor Sant'ana <igorba2005@gmail.com>
2016-06-23 20:02:59 -0300

    Introduction to algorithm

---
+++ test/test_algorithm.py
@@ -0,0 +1,9 @@
+import unittest
+import sys
+
+from hypercube.algorithm import DataStructure
+
+class DataStructureTest(unittest.TestCase):
+
+    def test_nothing(self):
+        self.assertEqual(1, 1)
